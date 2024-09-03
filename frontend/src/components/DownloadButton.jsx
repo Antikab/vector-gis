d2463b464814eb62b799345cdb2e083dab17d274
Arseny Prosvetov <arsprosvet@gmail.com>
2024-09-03 19:46:58 +0300

    progress

--- frontend/src/components/DownloadButton.jsx
+++ frontend/src/components/DownloadButton.jsx
@@ -4,40 +4,38 @@ import { useState } from 'react';
 function DownloadButton({ url, fileName }) {
 	const [isDownloading, setIsDownloading] = useState(false);
 
-const downloadFile = async () => {
-  setIsDownloading(true);
+	const downloadFile = async () => {
+		setIsDownloading(true);
 
-  try {
-    const response = await axios.get(url, {
-      responseType: 'blob', // Получаем данные в виде Blob
-    });
+		try {
+			const response = await axios.get(url, {
+				responseType: 'blob', // Получаем данные в виде Blob
+			});
 
-    // Создаем URL для Blob
-    const blobUrl = window.URL.createObjectURL(new Blob([response.data]));
+			// Создаем URL для Blob
+			const blobUrl = window.URL.createObjectURL(new Blob([response.data]));
 
-    // Создаем временную ссылку для скачивания файла
-    const link = document.createElement('a');
-    link.href = blobUrl;
-    link.setAttribute('download', fileName);
-    document.body.appendChild(link);
-    link.click();
+			// Создаем временную ссылку для скачивания файла
+			const link = document.createElement('a');
+			link.href = blobUrl;
+			link.setAttribute('download', fileName);
+			document.body.appendChild(link);
+			link.click();
 
-    // Чистим URL и ссылку
-    link.remove();
-    window.URL.revokeObjectURL(blobUrl);
-  } catch (error) {
-    console.error('Error downloading the file:', error);
-    alert('Ошибка при скачивании файла. Пожалуйста, попробуйте еще раз.');
-  } finally {
-    setIsDownloading(false);
-  }
-};
-
+			// Чистим URL и ссылку
+			link.remove();
+			window.URL.revokeObjectURL(blobUrl);
+		} catch (error) {
+			console.error('Error downloading the file:', error);
+		} finally {
+			setIsDownloading(false);
+		}
+	};
 
 	return (
 		<div>
 			<button
-      className='button'
+				className="button"
 				onClick={downloadFile}
 				disabled={isDownloading}
 			>
